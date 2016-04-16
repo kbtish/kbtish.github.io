53a808797d84e37b9e430863baf81b8320561f10
Kevin Bartish <kbtish@aol.com>
2016-04-16 14:00:27 -0500

    4/16/16 Added an external javascript file -- select or clear all digits for math facts flashcards

---
+++ external javascript files/selectDigits.js
@@ -0,0 +1,31 @@
+/*
+ * allow the user to practice with particular digits;
+ * these functions are called on button clicks to either select all digits 1-9 or clear all digits
+ */
+
+function selectAllDigits() {
+    document.forms[0].digitZero.checked = true;
+    document.forms[0].digitOne.checked = true;
+    document.forms[0].digitTwo.checked = true;
+    document.forms[0].digitThree.checked = true;
+    document.forms[0].digitFour.checked = true;
+    document.forms[0].digitFive.checked = true;
+    document.forms[0].digitSix.checked = true;
+    document.forms[0].digitSeven.checked = true;
+    document.forms[0].digitEight.checked = true;
+    document.forms[0].digitNine.checked = true;
+}
+
+function clearAllDigits() {
+    document.forms[0].digitZero.checked = false;
+    document.forms[0].digitOne.checked = false;
+    document.forms[0].digitTwo.checked = false;
+    document.forms[0].digitThree.checked = false;
+    document.forms[0].digitFour.checked = false;
+    document.forms[0].digitFive.checked = false;
+    document.forms[0].digitSix.checked = false;
+    document.forms[0].digitSeven.checked = false;
+    document.forms[0].digitEight.checked = false;
+    document.forms[0].digitNine.checked = false;
+}
+
