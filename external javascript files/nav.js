6f0aa29e90f98e5ba277b403ecc8ab9937985eb3
kbtish@aol.com <kbtish@aol.com>
2016-09-25 22:15:50 -0500

    9/25/16 Added link in nav section to "All flashcards"

--- external javascript files/nav.js
+++ external javascript files/nav.js
@@ -6,4 +6,4 @@ document.getElementById('nav').innerHTML =
     "<a href='http://kbtish.github.io/Whole%20Numbers/whole_numbers_main.html'>Whole Numbers</a><br />" + 
     "<a href='http://kbtish.github.io/Whole%20Numbers/fractions_decimals_main.html'>Fractions/Decimals</a><br />" +
     "<a href='http://kbtish.github.io/Whole%20Numbers/prealgebra_main.html'>Pre-Algebra</a><br />" +
-    "<a href='http://kbtish.github.io/Whole%20Numbers/all_flaschards.html'>All Flashcards</a>";
+    "<a href='http://kbtish.github.io/Whole%20Numbers/all_flashcards.html'>All Flashcards</a>";
