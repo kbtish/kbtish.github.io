6c357014f6a834a28aef7f0d14a37938a8b178a9
kbtish@aol.com <kbtish@aol.com>
2016-10-15 09:19:27 -0500

    10/15/16 Added a welcome message to flash cards pages informing users of the options on the right.  Made some changes to default setting on certain flash cards pages.

---
+++ external javascript files/welcome_options.js
@@ -0,0 +1,9 @@
+/* Welcome message for flash cards
+ *
+ * An alert window will inform users that they can customize the page with the options on the right
+*/
+
+// give a welcome message when the page loads the first time
+function welcome() {
+    window.alert('Customize your practice with the options on the right.');
+}
